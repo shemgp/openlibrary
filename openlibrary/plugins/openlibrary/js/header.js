1abc8da37f30792c38f6d32a251838b13570fc55
Michael E. Karpeles <michael.karpeles@gmail.com>
2017-04-25 21:31:00 +0000

    replacing OL nav bar with new concept placeholder

---
+++ openlibrary/plugins/openlibrary/js/header.js
@@ -0,0 +1,14 @@
+/*
+$(document).ready(function(){
+
+    $('.search_text').focusin(function(e) {
+	$('.searchbar').addClass('active');
+	$('#search-dropdown').toggle();
+    });
+
+    $('.search_text').blur(function(e) {
+	$('.searchbar').removeClass('active');
+	$('#search-dropdown').toggle();
+    });
+});
+*/
